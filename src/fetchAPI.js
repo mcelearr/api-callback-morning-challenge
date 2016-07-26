ee203df44eea68461c9ee3e6ef096a82435ff546
Nogaenbar <noga.inbar@gmail.com>
2016-07-26 12:32:41 +0100

    creates a https module and makes a calback hell2

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -2,18 +2,10 @@ const httpsRequest = require('./httpsRequest.js').httpsRequest;
 
 httpsRequest('maps.googleapis.com',`/maps/api/geocode/json?address=14+palmers+road,
 +London,+e20sy&key=AIzaSyA-CPghzSNJEsGODSZcKdeBvG-pTA6UqNM`, (response) => {
+  console.log(response);
   httpsRequest('www.metaweather.com','/api/location/search/?lattlong=51.5295318,-0.0422949',(response) => {
     httpsRequest('www.metaweather.com','/api/location/44418/', (response) => {
       console.log(response);
     });
   });
 });
-
-//
-
-// const renderData = function(options, cb){
-//   getGeoCodedLocation(options, cb){
-//     getWeather(options, cb){
-//     }
-//   }
-// };
